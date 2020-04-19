2aa718b1e6985b691a90d5917af9e295ab377f4c
Bruno Lacerda <brunolcpg@gmail.com>
2020-04-19 20:51:23 -0300

    Implements get and post function to /transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,33 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionRepository.save(transaction);
+    return transaction;
   }
 }
 
