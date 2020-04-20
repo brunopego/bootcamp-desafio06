28bfbbfc133c85fca3ab2ec43671ba5d0ed2f512
Bruno Lacerda <brunolcpg@gmail.com>
2020-04-19 22:55:03 -0300

    Persists category when creating transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,8 +1,10 @@
-// import AppError from '../errors/AppError';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 import TransactionRepository from '../repositories/TransactionsRepository';
+import CreateCategoryService from './CreateCategoryService';
 
 interface Request {
   title: string;
@@ -19,11 +21,31 @@ class CreateTransactionService {
     category,
   }: Request): Promise<Transaction> {
     const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
 
-    const transaction = await transactionRepository.create({
+    if (type === 'outcome') {
+      const balance = await transactionRepository.getBalance();
+      if (balance.total < value) {
+        throw new AppError("You don't have enough income");
+      }
+    }
+
+    let categoryTransaction = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryTransaction) {
+      const createCategoryService = new CreateCategoryService();
+      categoryTransaction = await createCategoryService.execute({
+        title: category,
+      });
+    }
+
+    const transaction = transactionRepository.create({
       title,
       value,
       type,
+      category_id: categoryTransaction.id,
     });
 
     await transactionRepository.save(transaction);
