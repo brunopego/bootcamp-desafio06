d7ac51aa9bc393a8fd0fb0bf5f7a709251b666cb
Bruno Lacerda <brunolcpg@gmail.com>
2020-04-19 22:01:49 -0300

    Implements getBalance function for transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,8 +11,31 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    const balance: Balance = { income: 0, outcome: 0, total: 0 };
-    return balance;
+    const transactions = await this.find();
+
+    const income = this.getTotalIncome(transactions);
+    const outcome = this.getTotalOutcome(transactions);
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
+
+  private getTotalIncome(transactions: Transaction[]): number {
+    const income = transactions.reduce((accum, transaction) => {
+      return transaction.type === 'income'
+        ? accum + Number(transaction.value)
+        : accum;
+    }, 0);
+    return income || 0;
+  }
+
+  private getTotalOutcome(transactions: Transaction[]): number {
+    const outcome = transactions.reduce((accum, transaction) => {
+      return transaction.type === 'outcome'
+        ? accum + Number(transaction.value)
+        : accum;
+    }, 0);
+    return outcome || 0;
   }
 }
 
